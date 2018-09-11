097af10dcf2e82a2f7acc67caaf3c62f1f2eb6b9
Sharma, Kartikay <sharmaskartik@gmail.com>
2018-09-10 18:55:24 -0600

    Initial Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='reveal',
+      version='0.1',
+      description='Experiments to observe the inner workings of deep neural networks',
+      author='Kartikay Sharma',
+      author_email='kartikay@colostate.edu',
+      license='MIT',
+      packages=['reveal'],
+      zip_safe=False)
