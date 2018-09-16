3796e378ba97472f30658f6a01e3aa36820e9b04
Sharma, Kartikay <sharmaskartik@gmail.com>
2018-09-15 22:09:30 -0600

    added class structure to the LossComparision experiment

---
+++ reveal/experiments/performance/__init__.py
@@ -0,0 +1,1 @@
+from .loss_comparision import LossComparision
--- reveal/experiments/performance/loss_comparision.py
+++ reveal/experiments/performance/loss_comparision.py
@@ -1,5 +1,6 @@
 import reveal.data_utils.partition_data as partition
 import reveal.data_utils.data_processor as dp
+
 from reveal.networks.feedforward import *
 
 import torch
@@ -7,54 +8,111 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-def compareActivations(X, T, list_of_activations, n_iterations, batch_size):
-
-    net = TwoLayerFF([X.shape[1],10,10,T.shape[1]], F.relu)
-    criterion = nn.MSELoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    print(net)
-
-    X, T = dp._convertFromNumpy([X, T], torch.FloatTensor)
-
-    partitions = partition.partition(X, T, [0.8, 0.2], shuffle = True)
-
-    partitions, unstd_t = dp._standardize_train_test(partitions)
-
-    Xtrain, Ttrain, Xtest, Ttest = partitions
-
-    n_train_samples = Xtrain.size()[0]
-    for epoch in range(n_iterations):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i in range(0, n_train_samples, batch_size):
-            if n_train_samples - i < batch_size:
-                end = n_train_samples
-            else:
-                end = i + batch_size
-
-            X_iter = Xtrain[i:end,:].unsqueeze(0)
-            T_iter = Ttrain[i:end,:].unsqueeze(0)
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs, _ = net(X_iter)
-            loss = criterion(outputs, T_iter)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-        print(running_loss)
-        running_loss = 0.0
-
-    print('Finished Training')
+class LossComparision():
 
 
-    print('beginning testing')
+    def __init__(self, *args, **kwargs):
+        super(LossComparision, self).__init__()
+
+        self.params = {}
+        self.params['repetition'] = 10
+        self.params['epochs'] = 10
+        self.params['batch_size'] = 10
+
+        self.params['problem_type'] = None
+        self.params['net_structures'] = None
+        self.params['network_type'] = None
+        self.params['activations'] = None
+        self.params['loss_function'] = None
+        self.params['optimizer'] = None
 
 
-    output_test, _ = net(Xtest.unsqueeze(0))
-    output_test = unstd_t(output_test)
-    mse = criterion(output_test, Ttest.unsqueeze(0)).item()
-    print("test error", mse)
+    def _check_parameters(self):
+
+        stat = 0
+
+        if self.params['problem_type'] is None:
+            print("params['problem_type'] was not set")
+            stat = -1
+
+        if self.params['net_structures'] is None:
+            print("params['net_structures'] was not set")
+            stat = -1
+
+        if self.params['network_type'] is None:
+            print("params['network_type'] was not set")
+            stat = -1
+
+        if self.params['activations'] is None:
+            print("params['activations'] was not set")
+            stat = -1
+
+        if self.params['loss_function'] is None:
+            print("params['loss_function'] was not set")
+            stat = -1
+
+        if self.params['optimizer'] is None:
+            print("params['optimizer'] was not set")
+            stat = -1
+
+        return stat
+
+    def compare_loss(self, X, T):
+
+        #check if the parameters are valid
+        stat = self._check_parameters()
+
+        if stat < 0:
+            return
+
+        net = TwoLayerFF([X.shape[1],10,10,T.shape[1]], F.relu)
+        criterion = nn.MSELoss()
+        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+        print(net)
+
+        X, T = dp._convertFromNumpy([X, T], torch.FloatTensor)
+
+        partitions = partition.partition(X, T, [0.8, 0.2], shuffle = True)
+
+        partitions, unstd_t = dp._standardize_train_test(partitions)
+
+        Xtrain, Ttrain, Xtest, Ttest = partitions
+
+        n_train_samples = Xtrain.size()[0]
+
+        batch_size = self.params['batch_size']
+        for epoch in range(self.params['epochs']):  # loop over the dataset multiple times
+
+            running_loss = 0.0
+            for i in range(0, n_train_samples, batch_size):
+                if n_train_samples - i < batch_size:
+                    end = n_train_samples
+                else:
+                    end = i + batch_size
+
+                X_iter = Xtrain[i:end,:].unsqueeze(0)
+                T_iter = Ttrain[i:end,:].unsqueeze(0)
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs, _ = net(X_iter)
+                loss = criterion(outputs, T_iter)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+            print(running_loss)
+            running_loss = 0.0
+
+        print('Finished Training')
+
+
+        print('beginning testing')
+
+
+        output_test, _ = net(Xtest.unsqueeze(0))
+        output_test = unstd_t(output_test)
+        mse = criterion(output_test, Ttest.unsqueeze(0)).item()
+        print("test error", mse)
